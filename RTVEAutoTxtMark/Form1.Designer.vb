8cdb614f7d7471f1fd1d46e809a6ef844178c694
Panera76 <30965152+Panera76@users.noreply.github.com>
2018-04-24 04:09:37 +0200

    V1.0.0 - Versión operativa multifichero
    Versión para probar en producción. Soporta multifichero.

--- RTVEAutoTxtMark/Form1.Designer.vb
+++ RTVEAutoTxtMark/Form1.Designer.vb
@@ -23,33 +23,31 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.txtCarpetaEntrada = New System.Windows.Forms.TextBox()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.lblEstado = New System.Windows.Forms.Label()
         Me.FileSystemWatcher1 = New System.IO.FileSystemWatcher()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.txtCarpetaSalida = New System.Windows.Forms.TextBox()
         Me.btnSeleccionarCarpetaEntrada = New System.Windows.Forms.Button()
         Me.btnSeleccionarCarpetaSalida = New System.Windows.Forms.Button()
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.txtLog = New System.Windows.Forms.TextBox()
+        Me.txtCarpetaSalida = New System.Windows.Forms.TextBox()
+        Me.txtCarpetaEntrada = New System.Windows.Forms.TextBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblVersion = New System.Windows.Forms.Label()
         CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'txtCarpetaEntrada
-        '
-        Me.txtCarpetaEntrada.Location = New System.Drawing.Point(117, 12)
-        Me.txtCarpetaEntrada.Name = "txtCarpetaEntrada"
-        Me.txtCarpetaEntrada.Size = New System.Drawing.Size(290, 20)
-        Me.txtCarpetaEntrada.TabIndex = 0
-        Me.txtCarpetaEntrada.Text = "C:\Users\Pat\Desktop\Watched"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Location = New System.Drawing.Point(12, 149)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(99, 13)
         Me.Label1.TabIndex = 1
@@ -58,7 +56,7 @@ Partial Class Form1
         'lblEstado
         '
         Me.lblEstado.AutoSize = True
-        Me.lblEstado.Location = New System.Drawing.Point(12, 203)
+        Me.lblEstado.Location = New System.Drawing.Point(12, 368)
         Me.lblEstado.Name = "lblEstado"
         Me.lblEstado.Size = New System.Drawing.Size(40, 13)
         Me.lblEstado.TabIndex = 3
@@ -75,23 +73,15 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 42)
+        Me.Label2.Location = New System.Drawing.Point(12, 176)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(91, 13)
         Me.Label2.TabIndex = 7
         Me.Label2.Text = "Carpeta de Salida"
         '
-        'txtCarpetaSalida
-        '
-        Me.txtCarpetaSalida.Location = New System.Drawing.Point(117, 39)
-        Me.txtCarpetaSalida.Name = "txtCarpetaSalida"
-        Me.txtCarpetaSalida.Size = New System.Drawing.Size(290, 20)
-        Me.txtCarpetaSalida.TabIndex = 8
-        Me.txtCarpetaSalida.Text = "C:\Users\Pat\Desktop\Watched"
-        '
         'btnSeleccionarCarpetaEntrada
         '
-        Me.btnSeleccionarCarpetaEntrada.Location = New System.Drawing.Point(413, 12)
+        Me.btnSeleccionarCarpetaEntrada.Location = New System.Drawing.Point(413, 146)
         Me.btnSeleccionarCarpetaEntrada.Name = "btnSeleccionarCarpetaEntrada"
         Me.btnSeleccionarCarpetaEntrada.Size = New System.Drawing.Size(33, 20)
         Me.btnSeleccionarCarpetaEntrada.TabIndex = 9
@@ -100,7 +90,7 @@ Partial Class Form1
         '
         'btnSeleccionarCarpetaSalida
         '
-        Me.btnSeleccionarCarpetaSalida.Location = New System.Drawing.Point(413, 39)
+        Me.btnSeleccionarCarpetaSalida.Location = New System.Drawing.Point(413, 173)
         Me.btnSeleccionarCarpetaSalida.Name = "btnSeleccionarCarpetaSalida"
         Me.btnSeleccionarCarpetaSalida.Size = New System.Drawing.Size(33, 20)
         Me.btnSeleccionarCarpetaSalida.TabIndex = 10
@@ -109,26 +99,73 @@ Partial Class Form1
         '
         'ProgressBar1
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(15, 219)
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 384)
         Me.ProgressBar1.Name = "ProgressBar1"
         Me.ProgressBar1.Size = New System.Drawing.Size(431, 23)
         Me.ProgressBar1.TabIndex = 11
         '
         'txtLog
         '
-        Me.txtLog.Enabled = False
-        Me.txtLog.Location = New System.Drawing.Point(15, 76)
+        Me.txtLog.Location = New System.Drawing.Point(15, 210)
         Me.txtLog.Multiline = True
         Me.txtLog.Name = "txtLog"
+        Me.txtLog.ReadOnly = True
         Me.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.txtLog.Size = New System.Drawing.Size(431, 124)
+        Me.txtLog.Size = New System.Drawing.Size(431, 155)
         Me.txtLog.TabIndex = 12
+        '
+        'txtCarpetaSalida
+        '
+        Me.txtCarpetaSalida.Location = New System.Drawing.Point(117, 173)
+        Me.txtCarpetaSalida.Name = "txtCarpetaSalida"
+        Me.txtCarpetaSalida.Size = New System.Drawing.Size(290, 20)
+        Me.txtCarpetaSalida.TabIndex = 8
+        Me.txtCarpetaSalida.Text = Global.RTVEAutoTxtMark.My.MySettings.Default.CarpetaSalida
+        '
+        'txtCarpetaEntrada
+        '
+        Me.txtCarpetaEntrada.Location = New System.Drawing.Point(117, 146)
+        Me.txtCarpetaEntrada.Name = "txtCarpetaEntrada"
+        Me.txtCarpetaEntrada.Size = New System.Drawing.Size(290, 20)
+        Me.txtCarpetaEntrada.TabIndex = 0
+        Me.txtCarpetaEntrada.Text = Global.RTVEAutoTxtMark.My.MySettings.Default.CarpetaEntrada
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.RTVEAutoTxtMark.My.Resources.Resources.Logo_RTVE
+        Me.PictureBox2.Location = New System.Drawing.Point(292, 41)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(151, 83)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 14
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.RTVEAutoTxtMark.My.Resources.Resources.Logo_Doc_it
+        Me.PictureBox1.Location = New System.Drawing.Point(-1, 1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(230, 165)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 13
+        Me.PictureBox1.TabStop = False
+        '
+        'lblVersion
+        '
+        Me.lblVersion.AutoSize = True
+        Me.lblVersion.Location = New System.Drawing.Point(404, 410)
+        Me.lblVersion.Name = "lblVersion"
+        Me.lblVersion.Size = New System.Drawing.Size(37, 13)
+        Me.lblVersion.TabIndex = 15
+        Me.lblVersion.Text = "v1.0.0"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(458, 254)
+        Me.ClientSize = New System.Drawing.Size(458, 427)
+        Me.Controls.Add(Me.lblVersion)
+        Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.txtLog)
         Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.btnSeleccionarCarpetaSalida)
@@ -138,9 +175,15 @@ Partial Class Form1
         Me.Controls.Add(Me.lblEstado)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.txtCarpetaEntrada)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Text = "RTVE Generador de TXT automático"
         CType(Me.FileSystemWatcher1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -158,4 +201,7 @@ Partial Class Form1
     Friend WithEvents Label2 As Label
     Friend WithEvents txtLog As TextBox
     Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblVersion As Label
 End Class
--- RTVEAutoTxtMark/Form1.vb
+++ RTVEAutoTxtMark/Form1.vb
@@ -1,6 +1,5 @@
 ﻿'Fuente del código: http://www.recursosvisualbasic.com.ar/htm/vb-net/18-filesystemwatcher.htm
 
-Imports System
 Imports System.IO
 Imports System.Text
 Imports iTextSharp.text.pdf
@@ -10,6 +9,7 @@ Imports System.Text.RegularExpressions
 Public Class Form1
 
     Dim FicheroAbierto As String
+    Dim quFicherosAñadidos As New Queue(Of String)
 
     Public Function FileInUse(ByVal sFile As String) As Boolean
         If System.IO.File.Exists(sFile) Then
@@ -25,8 +25,8 @@ Public Class Form1
     End Function
 
     Private Sub FileSystemWatcher1_Created(ByVal sender As Object, ByVal e As System.IO.FileSystemEventArgs) Handles FileSystemWatcher1.Created
-        ' System.Threading.Thread.Sleep(1000) ' 1 segundo
-        ProcesarFichero(e.FullPath)
+        quFicherosAñadidos.Enqueue(e.FullPath)
+        ProcesarFicheros()
     End Sub
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
@@ -63,26 +63,44 @@ Public Class Form1
         End Try
     End Sub
 
-    Private Function ProcesarFichero(RutaFichero As String) As Integer
-        If FileInUse(RutaFichero) Then
-            FicheroAbierto = RutaFichero
+    Private Function ProcesarFicheros() As Integer
+
+        Dim PrimerFichero As String = quFicherosAñadidos(0)
+
+        If FileInUse(PrimerFichero) Then
             Timer1.Interval = 5000
             Timer1.Enabled = True
-            lblEstado.Text = "Esperando cierre del fichero " & IO.Path.GetFileName(RutaFichero)
+            lblEstado.Text = "Esperando cierre del fichero " & IO.Path.GetFileName(PrimerFichero)
         Else
-            ExtraerTXTdesdePDF(RutaFichero)
+            If quFicherosAñadidos.Count > 0 Then
+                quFicherosAñadidos.Dequeue()
+                ExtraerTXTdesdePDF(PrimerFichero)
+            End If
+            If quFicherosAñadidos.Count > 0 Then ProcesarFicheros()
         End If
     End Function
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        If FileInUse(FicheroAbierto) = False Then
+        Dim PrimerFichero As String = quFicherosAñadidos(0)
+
+        If FileInUse(PrimerFichero) = False Then
             Timer1.Enabled = False
             lblEstado.Text = "Fichero cerrado"
+            If quFicherosAñadidos.Count > 0 Then
+                quFicherosAñadidos.Dequeue()
+                ExtraerTXTdesdePDF(PrimerFichero)
+            End If
+            If quFicherosAñadidos.Count > 0 Then ProcesarFicheros()
         End If
     End Sub
 
-    Private Sub txtPath_LostFocus(sender As Object, e As EventArgs) Handles txtCarpetaEntrada.LostFocus
+    Private Sub txtCarpetaEntrada_LostFocus(sender As Object, e As EventArgs) Handles txtCarpetaEntrada.LostFocus
         cambiar_FSW(FileSystemWatcher1)
+        'My.Settings.CarpetaEntrada = txtCarpetaEntrada.Text
+    End Sub
+
+    Private Sub txtCarpetaSalida_LostFocus(sender As Object, e As EventArgs) Handles txtCarpetaSalida.LostFocus
+        'My.Settings.CarpetaSalida = txtCarpetaSalida.Text
     End Sub
 
     Private Sub btnSeleccionarCarpetaEntrada_Click(sender As Object, e As EventArgs) Handles btnSeleccionarCarpetaEntrada.Click
@@ -96,6 +114,7 @@ Public Class Form1
                 cambiar_FSW(FileSystemWatcher1)
             End If
         End With
+        ' My.Settings.CarpetaEntrada = txtCarpetaEntrada.Text
     End Sub
 
     Private Sub btnSeleccionarCarpetaSalida_Click(sender As Object, e As EventArgs) Handles btnSeleccionarCarpetaSalida.Click
@@ -108,6 +127,7 @@ Public Class Form1
                 .Dispose()
             End If
         End With
+        'My.Settings.CarpetaSalida = txtCarpetaSalida.Text
     End Sub
 
     Private Function ExtraerTXTdesdePDF(RutaNombreFicheroPDF As String) As Integer
@@ -122,16 +142,20 @@ Public Class Form1
         Dim RegExpr As String = "((?:(?:[1]{1}\d{1}\d{1}\d{1})|(?:[2]{1}\d{3}))(?:[0]?[1-9]|[1][012])(?:(?:[0-2]?\d{1})|(?:[3][01]{1})))(5D|PA|AB|CO|PE|MD|RZ|MO|MA|VG|SG|LX|TM|CA16|HL|SM)"
         Dim r As Regex = New Regex(RegExpr, RegexOptions.IgnoreCase Or RegexOptions.Singleline)
         If r.IsMatch(NombreFichero) = False Then
-            MsgBox("El fichero contiene un nombre erróneo")
+            lblEstado.Text = "En espera"
+            txtLog.Text += DateTime.Now & " - El fichero " & RutaNombreFicheroPDF & " contiene un nombre erróneo" & vbCrLf
+            Me.Refresh()
             Return -1
         End If
 
         Fecha = Strings.Left(NombreFichero, 8)
-        AcronimoPub = Strings.Right(NombreFichero, NombreFichero.Length - 8)
+        AcronimoPub = Strings.Right(NombreFichero, NombreFichero.Length - 8).ToUpper
         Publicacion = ObtenerPublicacion(AcronimoPub)
 
         If Publicacion = "Error" Then
-            MsgBox("El fichero contiene un nombre de publicación erróneo")
+            lblEstado.Text = "En espera"
+            txtLog.Text += DateTime.Now & " - El archivo " & RutaNombreFicheroPDF & " contiene un nombre erróneo" & vbCrLf
+            Me.Refresh()
             Return -2
         End If
 
@@ -140,7 +164,12 @@ Public Class Form1
         lblEstado.Refresh()
 
         Cadena = "%%" & Publicacion & " / " & Fecha & " / "
-
+        If My.Computer.FileSystem.FileExists(RutaNombreFicheroPDF) = False Then
+            lblEstado.Text = "En espera"
+            txtLog.Text += DateTime.Now & " - Error leyendo archivo " & RutaNombreFicheroPDF & vbCrLf
+            Me.Refresh()
+            Return -3
+        End If
         Dim LectorPDF As New PdfReader(RutaNombreFicheroPDF)
         NumPaginas = LectorPDF.NumberOfPages
 
@@ -161,22 +190,22 @@ Public Class Form1
             My.Computer.FileSystem.CreateDirectory(SubCarpetaDestino)
             File.WriteAllText(SubCarpetaDestino & "\" & Fecha & ".txt", TextoPDF, Encoding.UTF8)
             lblEstado.Text = "En espera"
-            txtLog.Text += "Generado fichero " & Fecha & ".txt de " & Publicacion & vbCrLf
+            txtLog.Text += DateTime.Now & " - Generado fichero " & Fecha & ".txt de " & Publicacion & vbCrLf
             Me.Refresh()
         Catch ex As Exception
-            MessageBox.Show("Error en la escritura del fichero " & SubCarpetaDestino & "\" & Fecha & ".txt" & " - Descripción: " & ex.Message)
+            'MessageBox.Show("Error en la escritura del fichero " & SubCarpetaDestino & "\" & Fecha & ".txt" & " - Descripción: " & ex.Message)
             lblEstado.Text = "En espera"
-            txtLog.Text += "Error en la generación del fichero " & Fecha & ".txt de " & Publicacion & vbCrLf
+            txtLog.Text += DateTime.Now & " - Error en la escritura del fichero " & SubCarpetaDestino & "\" & Fecha & ".txt de " & Publicacion & " - Descripción: " & ex.Message & vbCrLf
         End Try
 
         Try
-            My.Computer.FileSystem.MoveFile(RutaNombreFicheroPDF, SubCarpetaDestino & "\" & Fecha & ".pdf")
-            txtLog.Text += "Movido el fichero " & Fecha & ".pdf de " & Publicacion & vbCrLf
+            My.Computer.FileSystem.MoveFile(RutaNombreFicheroPDF, SubCarpetaDestino & "\" & Fecha & ".pdf", True)
+            txtLog.Text += DateTime.Now & " - Movido el fichero " & Fecha & ".pdf de " & Publicacion & vbCrLf
             Me.Refresh()
         Catch ex As Exception
-            MessageBox.Show("Error moviendo el fichero " & RutaNombreFicheroPDF & " - Descripción: " & ex.Message)
+            'MessageBox.Show("Error moviendo el fichero " & RutaNombreFicheroPDF & " - Descripción: " & ex.Message)
             lblEstado.Text = "En espera"
-            txtLog.Text += "Error moviendo el fichero " & RutaNombreFicheroPDF & vbCrLf
+            txtLog.Text += DateTime.Now & " - Error moviendo el fichero " & RutaNombreFicheroPDF & " - Descripción: " & ex.Message & vbCrLf
         End Try
 
     End Function
@@ -219,175 +248,5 @@ Public Class Form1
                 Return "Error"
         End Select
     End Function
-
-
-
-
-
-
-
-
-
-
-
-
-
-    '    ' Objeto NotifyIcon con evento para colocar el formulario en el tray  
-    '    Private WithEvents mNotifyIcon As New NotifyIcon
-
-
-    '    ' Subs y funciones  
-    '    ' ''''''''''''''''''''''''''''  
-
-    '    ' botón que abre el diálogo para seleccionar el directorio a monitorear  
-    '    Private Sub btnPath_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPath.Click
-
-    '        Dim oFolderBrowser As New FolderBrowserDialog
-
-    '        With oFolderBrowser
-    '            .SelectedPath = ""
-    '            If .ShowDialog = Windows.Forms.DialogResult.OK Then
-    '                txtPath.Text = .SelectedPath
-    '                .Dispose()
-    '                cambiar_FSW(FileSystemWatcher1)
-    '            End If
-    '        End With
-    '    End Sub
-
-    '    ' elimina en el mNotifyIcon  
-    '    Private Sub Form1_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
-    '        mNotifyIcon.Dispose()
-    '    End Sub
-
-
-    '    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-    '        ' inicializa las propiedades del FileSystemWatcher  
-    '        With FileSystemWatcher1
-    '            ' no incluye directorios en el monitoreo  
-    '            .IncludeSubdirectories = False
-    '            .EnableRaisingEvents = False
-    '            ' Monitoreas todos los archivos del directorio  
-    '            .Filter = "*.*"
-    '            ' filtros : Creación, cambios en el archivo ( sin directorios )  
-    '            .NotifyFilter = NotifyFilters.CreationTime Or
-    '                            NotifyFilters.Size Or
-    '                            NotifyFilters.FileName
-
-    '            'MsgBox(.NotifyFilter)  
-    '            chkActivar.Text = "Activar/Desactivar notificación"
-    '        End With
-
-    '        ' agrega columnas ( path, el tipo de cambio y la fecha)  
-    '        With ListView1
-    '            .View = View.Details
-    '            .Columns.Add("Archivos ", 300)
-    '            .Columns.Add("Tipo de modificación", 120)
-    '            .Columns.Add("fecha", 200)
-    '        End With
-
-    '        btnPath.Text = "Seleccionar directorio"
-
-    '        ' establece el ícono del form para NotifyIcon  
-    '        With mNotifyIcon
-    '            .Icon = Me.Icon
-    '        End With
-    '    End Sub
-
-    '    ' checkbox que activa o desactiva el FileSystemWatcher  
-    '    Private Sub chkActivar_CheckedChanged(ByVal sender As System.Object,
-    '                                          ByVal e As System.EventArgs) _
-    '                                          Handles chkActivar.CheckedChanged
-    '        cambiar_FSW(FileSystemWatcher1)
-    '    End Sub
-
-    '    ' cambia el path del FileSystemWatcher y lo habilita o deshabilita  
-    '    Sub cambiar_FSW(ByVal FSW As FileSystemWatcher)
-
-    '        Try
-    '            ' comprueba que el directorio existe  
-    '            If Directory.Exists(txtPath.Text) = True Then
-    '                With FSW
-    '                    .Path = txtPath.Text
-    '                    ' activa o desactiva el  FileSystemWatcher  
-    '                    .EnableRaisingEvents = chkActivar.Checked
-    '                End With
-    '            End If
-    '            ' error  
-    '        Catch sError As Exception
-    '            MsgBox(sError.Message, MsgBoxStyle.Critical)
-    '        End Try
-    '    End Sub
-
-    '    ' agrega los datos al listview ( el nombre del archivo   
-    '    ' modificado, el tipo de cambio y la fecha)  
-    '    Sub Notificar_Cambio(ByVal Path As String,
-    '                         ByVal tipo As String)
-
-    '        Dim oItem As New ListViewItem(Path)
-    '        With oItem
-    '            .SubItems.Add(tipo)
-    '            .SubItems.Add(Date.Now.ToString)
-    '            ListView1.Items.Add(oItem)
-    '        End With
-
-    '        With mNotifyIcon
-    '            .ShowBalloonTip(5000, tipo, Path, ToolTipIcon.Info)
-    '        End With
-
-    '    End Sub
-
-    '    'eventos para el control FileSystemWatcher  
-    '    '''''''''''''''''''''''''''''''''''''''''''''''''''''''''  
-
-    '    ' cambio en carpeta o archivos  
-    '    Private Sub FileSystemWatcher1_Changed(ByVal sender As System.Object,
-    '                                           ByVal e As System.IO.FileSystemEventArgs) _
-    '                                           Handles FileSystemWatcher1.Changed
-    '        Notificar_Cambio(e.Name, "Cambio")
-    '    End Sub
-
-    '    ' creación   
-    '    Private Sub FileSystemWatcher1_Created(ByVal sender As Object,
-    '                                           ByVal e As System.IO.FileSystemEventArgs) _
-    '                                           Handles FileSystemWatcher1.Created
-    '        Notificar_Cambio(e.Name, "Creación")
-    '    End Sub
-    '    ' eliminación de archivos o carpetas      
-    '    Private Sub FileSystemWatcher1_Deleted(ByVal sender As Object,
-    '                                           ByVal e As System.IO.FileSystemEventArgs) _
-    '                                           Handles FileSystemWatcher1.Deleted
-    '        Notificar_Cambio(e.Name, "Eliminación")
-    '    End Sub
-
-    '    ' evento de error  
-    '    Private Sub FileSystemWatcher1_Error(ByVal sender As Object,
-    '                                         ByVal e As System.IO.ErrorEventArgs) _
-    '                                         Handles FileSystemWatcher1.Error
-    '        MsgBox(e.GetException.ToString)
-    '    End Sub
-
-    '    ' eventos del componente NotifyIcon  
-    '    ' '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''  
-    '    Private Sub mNotifyIcon_Click(ByVal sender As Object,
-    '                                  ByVal e As System.EventArgs) _
-    '                                  Handles mNotifyIcon.Click
-    '        Me.Visible = True
-    '        Me.WindowState = FormWindowState.Normal
-    '    End Sub
-
-    '    ' hace visible el NotifyIcon cuando se minimiza el formulario  
-    '    Private Sub Form1_Resize(ByVal sender As Object,
-    '                             ByVal e As System.EventArgs) _
-    '                             Handles Me.Resize
-
-    '        Select Case Me.WindowState
-    '            Case FormWindowState.Minimized
-    '                Me.Visible = False
-    '                mNotifyIcon.Visible = True
-    '                'Case FormWindowState.Normal  
-    '                '    mNotifyIcon.Visible = False  
-    '                '    Me.Visible = True  
-    '        End Select
-    '    End Sub
 
 End Class
